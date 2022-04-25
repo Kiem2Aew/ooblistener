8506ce44a02cfbe59bc4fae0e6da205b526b5e3e
Kiem2Aew <75021808+Kiem2Aew@users.noreply.github.com>
2022-04-25 23:09:43 +0300

    Update utils.go

--- pkg/options/utils.go
+++ pkg/options/utils.go
@@ -11,7 +11,7 @@ const banner = `
   ___    ___  | |__  | | _  ___ | |_   ___  _ __    ___  _ __ 
  / _ \  / _ \ | '_ \ | || |/ __|| __| / _ \| '_ \  / _ \| '__|
 | (_) || (_) || |_) || || |\__ \| |_ |  __/| | | ||  __/| |   
- \___/  \___/ |_.__/ |_||_||___/ \__| \___||_| |_| \___||_|  
+ \___/  \___/ |_.__/ |_||_||___/ \__| \___||_| |_| \___||_|(.ru/.online) 
 `
 // Создание словаря из переданных программе аргументов вида: {"input":{"Domain":...}, "services":{"DnsPort":...},"debug":{"debug":...}}
 func CreateGroup(flagSet *goflags.FlagSet, groupName, description string, flags ...*goflags.FlagData) {
@@ -23,7 +23,4 @@ func CreateGroup(flagSet *goflags.FlagSet, groupName, description string, flags 
 
 func ShowBanner() {
 	gologger.Print().Msgf("%s\n", banner)
-	//gologger.Print().Msgf("\t\tAuthor: Kumurzhi G.M.\n\n")
-	gologger.Warning().Msgf("Use with caution. You are responsible for your actions\n")
-	gologger.Warning().Msgf("Developers assume no liability and are not responsible for any misuse or damage.\n")
 }
